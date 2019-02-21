d6743dab6c9e00cb05171134c58c5ae5bbd16d90
RLUAN <rluan@DESKTOP-J4SALKO.home>
2019-02-21 00:05:06 -0300

    commit madruga

--- hakuna/t.cpp
+++ hakuna/t.cpp
@@ -3,42 +3,55 @@
 using namespace std;
 
 #define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL);
-typedef long long ll; 
 
-inline string IntToString(ll a){
-    char x[100];
-    sprintf(x,"%lld",a); string s = x;
-    return s;
+const int maxn = 100000;
+const int maxnt = 4 * maxn;
+bool tree[maxnt];
+
+void update(int node, int l, int r, int ll, int rr) {
+	if (l > rr || ll > r) {
+		return;
+	}
+	if (l == ll and r == rr) {
+		tree[node] = !tree[node];
+		return;
+	}
+	int mid   = (l + r) >> 1;
+	int left  = node << 1;
+	int right = node << 1 | 1;
+	update(left, l, mid, ll, mid);
+	update(right, mid + 1, r, mid + 1, rr);
+	tree[node] = tree[left] + tree[right];
 }
+int query(int node, int l, int r, int ll, int rr) {
+	
+	if (ll <= l and r <= rr) {
+		return tree[node];
+	}
+	if (l > rr || ll > r) {
+		return 0;
+	}
+	int mid   = (l + r) >> 1;
+	int left  = node << 1;
+	int right = node <<1 | 1;
+	return query(left, l, mid, ll, mid) + query(right, mid + 1, r, mid + 1, rr);
 
-inline ll StringToInt(string a){
-    char x[100]; ll res;
-    strcpy(x,a.c_str()); sscanf(x,"%lld",&res);
-    return res;
 }
-
-inline string GetString(void){
-    char x[1000005];
-    scanf("%s",x); string s = x;
-    return s;
-}
-
-struct points {
-	int x;
-	int y;
-
-	points() : x(0), y(0) {}
-	points (int x, int y) : x(x), y(y) {};
-	void read() { scanf ("%d %d", &x, &y); }
-	void print() { cout << x << " " << y << endl; }
-};
-
-
 int main() {
 	fast_io
-	
-
-
+	int n, m, k, s, e;
+	cin >> n >> m;
+	for (int i = 1; i <= n; ++i) {
+		tree[i] = 0;
+	}
+	n++;
+	for (int i = 1; i <= m; ++i) {
+		cin >> k >> s >> e;
+		if (k == 0) {
+			update(1, 1, n, s, e);		
+		} else {
+			cout << query(1, 1, n, s, e) << endl;
+		}
+	}
 	return 0;
 }
-

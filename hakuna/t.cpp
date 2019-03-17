6de84731c80c2768c4b996a207bc8cccdad63a58
RLUAN <rluan@DESKTOP-J4SALKO.home>
2019-03-16 21:43:41 -0300

    update folders

--- hakuna/t.cpp
+++ hakuna/t.cpp
@@ -4,79 +4,71 @@ using namespace std;
 
 #define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL);
 
+int v[5 * 100000 + 7], tree[5 * 4 * 100000 + 7], n;
 
-int tree[4 * 100000 + 1];
-
-void push(int x) {
-	tree[x] = tree[x + x] + tree[x + x + 1];
-}
 
 void build(int x, int l, int r) {
 	if (l == r) {
-		tree[x] = 0;
+		tree[x] = v[l];
 		return;
 	}
 	int m = (l + r) >> 1;
 	build(x + x, l, m);
 	build(x + x + 1, m + 1, r);
-	push(x);
+	tree[x] = -1;
 }
-void modify(int x, int l, int r, int ll, int rr) {
+void modify(int x, int l, int r, int ix) {
 	if (l == r) {
-		tree[x] = !tree[x];
-		return;
-	}
-	if (ll > r || rr < l) {
+		if (tree[x] == 0) {
+			tree[x] = 1;
+		}
 		return;
 	}
 	int m = (l + r) >> 1;
-
-	if (rr <= m) {
-		modify(x + x, l, m, ll, m);
-		
-	} else if (ll > m) {
-		modify(x + x + 1, m + 1, r, m + 1, rr);
-
-	} else {
-		modify(x + x, l, m, ll, m);
-		modify(x + x + 1, m + 1, r, m + 1, rr);
-
+	if (ix <= m) {
+		modify(x + x, l, m, ix);
+	} else if (ix > m) {
+		modify(x + x + 1, m + 1, r, ix);
 	}
-	push(x);
+	if (tree[x + x] >= tree[x + x + 1]) {
+		tree[x] = x + x - n + 1;
+	} else {
+		tree[x] = x + x + 1 - n + 1;
+	}
 }
 int query(int x, int l, int r, int ll, int rr) {
+	if (ll > r || rr < l) {
+		return -1;
+	}
 	if (ll <= l && r <= rr) {
+		if (tree[x] == 0) return -1;
 		return tree[x];
 	}
 	int m = (l + r) >> 1;
 	if (rr <= m) {
-		return query(x + x, l, m, ll, m);
-
+		return query(x + x, l, m, ll, rr);
 	} else if (ll > m) {
-		return query(x + x + 1, m + 1, r, m + 1, rr);
-
-	} else {
-		return query(x + x, l, m, ll, m) + query(x + x + 1, m + 1, r, m + 1, rr);
-
+		return query(x + x + 1, m + 1, r, ll, rr);
 	}
 }
 
-
-
 int main() {
 	fast_io
-
-
-	int n, m;
+	
+	int m, k, idx;
 	cin >> n >> m;
-	int k, s, e;
+	for (int i = 0; i < n; ++i) {
+		cin >> v[i];
+	}
+	memset(tree, 0, sizeof 0);
 	build(1, 1, n);
-	for (int i = 1; i <= m; ++i) {
-		cin >> k >> s >> e;
+	for (int i = 0; i < m; ++i) {
+		cin >> k >> idx;
 		if (k == 0) {
-			modify(1, 1, n, s, e);
+			cout << query(1, 1, n, 1, n >> 1) << " ";
+			cout << query(1, 1, n, (n >> 1) + 1, (n >> 1) + 1 + idx) << endl;
 		} else {
-			cout << query(1, 1, n, s, e) << endl;
+			modify(1, 1, n, idx);
 		}
 	}
 	return 0;

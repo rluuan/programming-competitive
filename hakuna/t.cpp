17320625c2ef60712b823b71952be081006e6137
RLUAN <rluan@DESKTOP-J4SALKO.home>
2019-05-02 22:55:53 -0300

    update bitmask

--- hakuna/t.cpp
+++ hakuna/t.cpp
@@ -5,7 +5,7 @@ using namespace std;
 #define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL);
 
 
-vector <char> store; 
+vector <char> store = {'a', 'b', 'c'};
 
 void print_bitmask(int bitmask) {
 
@@ -24,7 +24,6 @@ void print_bitmask(int bitmask) {
 
 
 int main() {
-	store.push_back('a'); store.push_back('b'); store.push_back('c');
 	print_bitmask(store.size());
 
 	return 0;

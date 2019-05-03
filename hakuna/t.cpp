4d055927ed914bd7a57e7c01b524fcaa60a91563
RLUAN <rluan@DESKTOP-J4SALKO.home>
2019-05-02 22:49:55 -0300

    treino com bitmask

--- hakuna/t.cpp
+++ hakuna/t.cpp
@@ -4,72 +4,28 @@ using namespace std;
 
 #define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL);
 
-int v[5 * 100000 + 7], tree[5 * 4 * 100000 + 7], n;
+
+vector <char> store; 
+
+void print_bitmask(int bitmask) {
 
 
-void build(int x, int l, int r) {
-	if (l == r) {
-		tree[x] = v[l];
-		return;
-	}
-	int m = (l + r) >> 1;
-	build(x + x, l, m);
-	build(x + x + 1, m + 1, r);
-	tree[x] = -1;
-}
-void modify(int x, int l, int r, int ix) {
-	if (l == r) {
-		if (tree[x] == 0) {
-			tree[x] = 1;
+	for (int i = 0; i < (1 << bitmask); ++i) {
+		cout << "{ ";
+		
+		for (int j = 0; j < bitmask; ++j) {
+			if ((i & (1 << j)) > 0) {
+				cout << store[j] << " ";
+			}
 		}
-		return;
-	}
-	int m = (l + r) >> 1;
-	if (ix <= m) {
-		modify(x + x, l, m, ix);
-	} else if (ix > m) {
-		modify(x + x + 1, m + 1, r, ix);
-	}
-	if (tree[x + x] >= tree[x + x + 1]) {
-		tree[x] = x + x - n + 1;
-	} else {
-		tree[x] = x + x + 1 - n + 1;
+		cout << "}" << endl;
 	}
 }
-int query(int x, int l, int r, int ll, int rr) {
-	if (ll > r || rr < l) {
-		return -1;
-	}
-	if (ll <= l && r <= rr) {
-		if (tree[x] == 0) return -1;
-		return tree[x];
-	}
-	int m = (l + r) >> 1;
-	if (rr <= m) {
-		return query(x + x, l, m, ll, rr);
-	} else if (ll > m) {
-		return query(x + x + 1, m + 1, r, ll, rr);
-	}
-}
+
 
 int main() {
-	fast_io
-	
-	int m, k, idx;
-	cin >> n >> m;
-	for (int i = 0; i < n; ++i) {
-		cin >> v[i];
-	}
-	memset(tree, 0, sizeof 0);
-	build(1, 1, n);
-	for (int i = 0; i < m; ++i) {
-		cin >> k >> idx;
-		if (k == 0) {
-			cout << query(1, 1, n, 1, n >> 1) << " ";
-			cout << query(1, 1, n, (n >> 1) + 1, (n >> 1) + 1 + idx) << endl;
-		} else {
-			modify(1, 1, n, idx);
-		}
-	}
+	store.push_back('a'); store.push_back('b'); store.push_back('c');
+	print_bitmask(store.size());
+
 	return 0;
 }
